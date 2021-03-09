3d34d29d2979006432e3597cec770435bfa841cc
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-09 18:16:35 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ module.exports = function towelSort (matrix) {
     let k = 0;
     let arr=[];
     for(let i = 0; i < matrix.length;i++){
-        for(let j = 0; j < matrix[i].length;j++){
-            arr[k] = matrix[i][j];
-            k++;
+        if(i%2 != 0){
+            for(let j = matrix[i].length - 1; j >= 0;j--){
+                arr[k] = matrix[i][j];
+                k++;
+            }
+        }
+        else{
+            for(let j = 0; j < matrix[i].length;j++){
+                arr[k] = matrix[i][j];
+                k++;
+            }
         }
     }
   return arr;
